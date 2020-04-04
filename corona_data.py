56d6261740126772a336c0cddcdcffe9bbb97122
Uday <54357496+udaypratapyati@users.noreply.github.com>
2020-04-04 16:26:58 +0530

    Updated as per new table format

--- corona_data.py
+++ corona_data.py
@@ -32,7 +32,8 @@ logging.basicConfig(format=FORMAT, level=logging.DEBUG, filename='bot.log', file
 
 
 URL='https://www.mohfw.gov.in/'
-shortHeader=['S.No','ST','IN','FR','CR','DT']
+# shortHeader=['S.No','ST','IN','FR','CR','DT']
+shortHeader=['S.No','State','Conf','Cured','Dead']
 FILE_NAME='corona_india.json'
 extract_contents=lambda row:[x.text.replace('\n','') for x in row]
 
@@ -58,10 +59,10 @@ def get_data_for_state(state):
         interested_state=[]
 
         if isinstance(state, str):
-#            print('string')
+            # print('string')
             interested_state = [state]
         else:
-#            print('list')
+            # print('list')
             interested_state = state
     
         current_time=datetime.datetime.now().strftime('%d/%m/%Y %H:%M')
@@ -75,26 +76,36 @@ def get_data_for_state(state):
     
         #gerting all the table rows from website
         all_rows=soup.find_all('tr')
-#        print(interested_state, len(interested_state))
+        # print(interested_state, len(interested_state))
+        # print(len(all_rows))
+        last_index = len(all_rows)
         for row in all_rows:
             stat= extract_contents(row.find_all('td'))
+            # print('stat len :', len(stat), stat)
             if stat:
                 if len(stat) < 3:
+                    # print('con')
                     continue
                       
-                if len(stat)==5:
+                if len(stat)==4:
                     #last row
                     stat=['',*stat]
                     stat[1]='Total'
+                    stat[0]=int(last_index) + 1
                     states.append(stat)
+                    # print(len(states))
                 elif any([ s.lower()  in  stat[1].lower() for s in interested_state]):
                     states.append(stat)
+                    # print(len(states))
+                    last_index = stat[0]
     
         # for state in states: {state[1]:{currenttime:[2:]}}#{statename:}
         cur_data ={x[1]:{current_time:x[2:]} for x in states} 
-#        print(current_time)
-#        print(cur_data)
-         
+        # print(current_time)
+        # print(cur_data)
+        # print(len(states))
+        # print(states) 
+        
         past_data=load()
         
         #initilaising boolean to chechk wether data change or not 
@@ -127,34 +138,42 @@ def get_data_for_state(state):
             telegram_bot_sendtext  ='\n-'+event.replace("'","")
         
         divideby = 1
+        # shortHeader = ['ST', 'IN', 'FR', 'CR', 'DT']
+        # print('headers : ', shortHeader)
         df = pd.DataFrame(states, columns=shortHeader) 
         df.set_index('S.No', inplace = True) 
-        if len(df) > 10:
+        
+        # print("len :", len(df))
+        if int(len(df)) > int(10):
             divideby = 2
         
-        events_info='\n IN -> Indian\n FR -> Foreigner\n CR -> Cured\n DT -> Deaths'
-        report = []       
-        df1 = df.iloc[:len(df)//divideby].copy()
-#        df1.reset_index(inplace=True)
-#        print("===> {}\n{}".format(len(df1), df1))
+        events_info= '' #'\n IN -> Indian\n FR -> Foreigner\n CR -> Cured\n DT -> Deaths'
+        report = []      
+        length = len(df)//divideby 
+        df1 = df.iloc[:length].copy()
+        # print("divideby : ", divideby)
+        # print(df1)
+
 #        table = tabulate(states, headers=shortHeader, tablefmt='orgtbl')
         table = tabulate(df1, headers=shortHeader, tablefmt='orgtbl')
         slack_text = f'Please find CoronaVirus Summary for India below:{events_info}\n```{table}```'
-#        print(slack_text)
+        print(slack_text)        
         report.append(slack_text)
         
-        if divideby > 1:
-            df2 = df.iloc[len(df)//divideby:].copy()
-#            df2.reset_index(inplace=True)
-#            print("===> {}\n{}".format(len(df2), df2))
+        # print("length : ", length)
+        if int(divideby) > int(1):
+            df2 = df.iloc[int(length):].copy()
+            # print(df2)
+
             table1 = tabulate(df2, headers=shortHeader, tablefmt='orgtbl')
             slack_text1 = f'Please find CoronaVirus Summary for India below:{events_info}\n```{table1}```'
-#            print(slack_text1)
+            print(slack_text1)
             report.append(slack_text1)
 #        telegram_bot_sendtext(slack_text)
         return report
                 
     except Exception as e:
         logging.exception('script failed',e)
-                    
-#print(get_data_for_state(''))
+
+states = ['Andhra Pradesh', 'Bihar', 'Chhattisgarh', 'Delhi', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Karnataka', 'Kerala', 'Madhya Pradesh', 'Maharashtra', 'Odisha', 'Puducherry', 'Punjab', 'Rajasthan', 'Tamil Nadu', 'Telengana', 'Chandigarh', 'Jammu and Kashmir', 'Ladakh', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal']                    
+(get_data_for_state(states))
